131c5a0ba4ab2b3ee5ca071b7f1e0610dc030304
Praveen Sankhla <pk54451636465@gmail.com>
2024-01-10 06:20:20 +0530

    Tip Calculator

---
+++ PriceQuatation/Server/Controllers/TipCalculatorController.cs
@@ -0,0 +1,29 @@
+﻿using TipCalculator.Shared;
+
+namespace PriceQuatation.Server.Controllers
+{
+    public interface IPriceQuatation
+    {
+        Task<TipCalculatorModel> Calculate(TipCalculatorModel PQ);
+    }
+
+    public class TipCalculatorController : IPriceQuatation
+    {
+        public async Task<TipCalculatorModel> Calculate(TipCalculatorModel PQ)
+        {
+            TipCalculatorModel PQDATA = new();
+            try
+            {
+                PQDATA.DiscountPercent_15 = (PQDATA.Costofmeal * 15) / 100;
+                PQDATA.DiscountPercent_20 = (PQDATA.Costofmeal * 20) / 100;
+                PQDATA.DiscountPercent_25 = (PQDATA.Costofmeal * 25) / 100;
+                return PQDATA;
+            }
+            catch (Exception)
+            {
+                PQDATA = new TipCalculatorModel();
+            }
+            return PQDATA;
+        }
+    }
+}
---
+++ PriceQuatation/Shared/TipCalculatorModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TipCalculator.Shared
+{
+    public class TipCalculatorModel
+    {
+        [Required(ErrorMessage = "Please Enter Cost of Meal")]
+        [Range(1, int.MaxValue, ErrorMessage = "Meal cost must be greater than 1")]
+        public Int32 Costofmeal { get; set; }
+
+        public Double DiscountPercent_15 { get; set; }
+        public Double DiscountPercent_20 { get; set; } = 0.00;
+        public Double DiscountPercent_25 { get; set; } = 0.00;
+    }
+}
